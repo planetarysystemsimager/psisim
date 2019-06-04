ac67ec7591fedc017254ed8676269a55ea80fed4
Jason Wang <jason.wang@outlook.com>
2019-06-03 20:51:48 -0700

    make planets random in example

--- psisim/Tutorials/example_yield_calculation.py
+++ psisim/Tutorials/example_yield_calculation.py
@@ -17,6 +17,7 @@ planet_types = []
 planet_spectra = []
 
 n_planets_now = 100
+rand_planets = np.random.randint(0, n_planets, n_planets_now)
 
 ########### Model spectrum wavelength choice #############
 # We're going to generate a model spectrum at a resolution twice the 
@@ -37,12 +38,14 @@ n_model_wv = int((model_wv_high-model_wv_low)/(dwv_c/2))*2
 model_wvs = np.linspace(model_wv_low, model_wv_high, n_model_wv) #Choose some wavelengths
 
 print("\n Starting to generate planet spectra")
-for planet in planet_table[:n_planets_now]:
+for planet in planet_table[rand_planets]:
+    #INSERT PLANET SELECTION RULES HERE
+    planet_type = "Gas"
+    planet_types.append(planet_type)
 
-	#INSERT PLANET SELECTION RULES HERE
-	planet_type = "Gas"
-	planet_types.append(planet_type)
-
+	#Generate the spectrum and downsample to intermediate resolution
+    atmospheric_parameters = spectrum.generate_picaso_inputs(planet,planet_type, clouds=True)
+    planet_spectrum = spectrum.simulate_spectrum(planet, model_wvs, intermediate_R, atmospheric_parameters)
     planet_spectra.append(planet_spectrum)
 
 print("Done generating planet spectra")
@@ -50,7 +53,7 @@ print("\n Starting to simulate observations")
 
 post_processing_gain=1000
 sim_F_lambda, sim_F_lambda_errs,sim_F_lambda_stellar, noise_components = observation.simulate_observation_set(tmt, psi_blue,
-	planet_table[:n_planets_now], planet_spectra, model_wvs, intermediate_R, inject_noise=False,
+	planet_table[rand_planets], planet_spectra, model_wvs, intermediate_R, inject_noise=False,
 	post_processing_gain=post_processing_gain,return_noise_components=True)
 
 speckle_noises = np.array([s[0] for s in noise_components])
@@ -60,12 +63,12 @@ flux_ratios = sim_F_lambda/sim_F_lambda_stellar
 detection_limits = sim_F_lambda_errs/sim_F_lambda_stellar
 snrs = sim_F_lambda/sim_F_lambda_errs
 
-detected = psi_blue.detect_planets(planet_table[:n_planets_now],snrs,tmt)
+detected = psi_blue.detect_planets(planet_table[rand_planets],snrs,tmt)
 
 #Choose which wavelength you want to plot the detections at:
 wv_index = 10
-fig, ax = plots.plot_detected_planet_contrasts(planet_table[:n_planets_now],wv_index,
-	detected,flux_ratios,psi_blue,tmt,ymin=1e-13,alt_data=5*detection_limits,alt_label=r"5-$\sigma$ Detection Limits")
+fig, ax = plots.plot_detected_planet_contrasts(planet_table[rand_planets],wv_index,
+	detected,flux_ratios,psi_blue,tmt,ymin=1e-13,alt_data=5*detection_limits,alt_label=r"5-$\sigma$ Detection Limits", show=False)
 
 #The user can now adjust the plot as they see fit. 
 #e.g. Annotate the plot
@@ -73,12 +76,6 @@ ax.text(4e-2,1e-5,"Planets detected: {}".format(len(np.where(detected[:,wv_index
 ax.text(4e-2,0.5e-5,"Planets not detected: {}".format(len(np.where(~detected[:,wv_index])[0])),color='k')
 ax.text(4e-2,0.25e-5,"Post-processing gain: {}".format(post_processing_gain),color='k')
 
-
-plt.figure()
-plt.scatter(seps,speckle_noises[:,10],label="Speckle Noise")
-plt.scatter(seps,photon_noises[:,10],label="Photon Noise")
-# plt.xscale('log')
-plt.legend()
 
 # get the best SNR Planet
 avg_snrs = np.mean(snrs, axis=1)
@@ -91,9 +88,9 @@ atmospheric_parameters_clear = spectrum.generate_picaso_inputs(planet, planet_ty
 planet_spectrum_clear = spectrum.simulate_spectrum(planet, model_wvs, intermediate_R, atmospheric_parameters_clear)
 
 # Generate noisy spectra for cloudy and clear
-clear_F_lambda, clear_F_lambda_errs = observation.simulate_observation(tmt, psi_blue,
+clear_F_lambda, clear_F_lambda_errs, _ = observation.simulate_observation(tmt, psi_blue,
    planet_table[rand_planets[bestsnr]], planet_spectrum_clear, model_wvs, intermediate_R, inject_noise=True,post_processing_gain=1000)
-cloudy_F_lambda, cloudy_F_lambda_errs = observation.simulate_observation(tmt, psi_blue,
+cloudy_F_lambda, cloudy_F_lambda_errs, _ = observation.simulate_observation(tmt, psi_blue,
    planet_table[rand_planets[bestsnr]], planet_spectra[bestsnr], model_wvs, intermediate_R, inject_noise=True,post_processing_gain=1000)
 
 fig = plt.figure()
