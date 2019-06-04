3ef483e5c9204c6ec832fc00ed681e2f4137c012
Jason Wang <jason.wang@outlook.com>
2019-06-03 20:50:50 -0700

    add in thermal noise from sky

--- psisim/telescope.py
+++ psisim/telescope.py
@@ -1,77 +1,115 @@
 import numpy as np
-
+import astropy.units as u
+import astropy.constants as constants
 
 class Telescope():
-	'''
-	A general class that describes a telescope and site
+    '''
+    A general class that describes a telescope and site
 
-	The main properties will be: 
-	diameter	- The telescope diameter in meters
-	collecting_area	- The telescope collecting area in meters
-	
-	Properties to consider adding later may be seeing, R0, Tau, etc. 
+    The main properties will be: 
+    diameter    - The telescope diameter in meters
+    collecting_area    - The telescope collecting area in meters
+    
+    Properties to consider adding later may be seeing, R0, Tau, etc. 
 
-	The main functions will be: 
-	get_sky_background()	- A function to get the sky background
-	get_atmospheric_transmission()	- A function to get the atmospheric transmission
+    The main functions will be: 
+    get_sky_background()    - A function to get the sky background
+    get_atmospheric_transmission()    - A function to get the atmospheric transmission
 
-	'''
-	def __init__(self,diameter, collecting_area=None):
-		'''
-		Constructor
+    '''
+    def __init__(self,diameter, collecting_area=None):
+        '''
+        Constructor
 
-		'''
+        '''
 
-		self.diameter = diameter
+        self.diameter = diameter
 
-		#If no collecting area is passed, be naive and assume it's a full circular mirror
-		if collecting_area is not None:
-			self.collecting_area = collecting_area
-		else: 
-			self.collecting_area = np.pi*(self.diameter/2)**2
+        #If no collecting area is passed, be naive and assume it's a full circular mirror
+        if collecting_area is not None:
+            self.collecting_area = collecting_area
+        else: 
+            self.collecting_area = np.pi*(self.diameter/2)**2
 
-	def get_sky_background(self,wvs):
-		'''
-		A function that returns the sky background for a given set of wavelengths. 
+    def get_sky_background(self,wvs):
+        '''
+        A function that returns the sky background for a given set of wavelengths. 
 
-		Later it might be a function of pressure, temperature and humidity
+        Later it might be a function of pressure, temperature and humidity
 
-		Inputs: 
-		wvs 	- A list of wavelengths 
+        Inputs: 
+        wvs     - A list of wavelengths 
 
-		Outputs: 
-		backgrounds - A list of backgrounds
-		'''
-		
-		if isinstance(wvs,float):
-			return 0
-		else:
-			return np.zeros(len(wvs))
+        Outputs: 
+        backgrounds - A list of backgrounds
+        '''
+        
+        if isinstance(wvs,float):
+            return 0
+        else:
+            return np.zeros(len(wvs))
 
 
-	def get_atmospheric_transmission(self,wvs):
-		'''
-		A function that returns the atmospheric transmission for a given set of wavelengths. 
+    def get_atmospheric_transmission(self,wvs):
+        '''
+        A function that returns the atmospheric transmission for a given set of wavelengths. 
 
-		Later it might be a function of pressure, temperature and humidity
+        Later it might be a function of pressure, temperature and humidity
 
-		Inputs: 
-		wvs 	- A list of wavelengths 
+        Inputs: 
+        wvs     - A list of wavelengths 
 
-		Outputs: 
-		transmissions - A list of atmospheric transmissions
-		'''
+        Outputs: 
+        transmissions - A list of atmospheric transmissions
+        '''
 
-		if isinstance(wvs,float):
-			return 1
-		else:
-			return np.ones(len(wvs))
+        if isinstance(wvs,float):
+            return 1
+        else:
+            return np.ones(len(wvs))
 
 
 
 class TMT(Telescope):
-	'''
-	An implementation of the Telescope class
-	'''
-	def __init__(self):
-		super(TMT, self).__init__(30)
+    '''
+    An implementation of the Telescope class
+    '''
+    def __init__(self):
+        super(TMT, self).__init__(30)
+
+    def get_sky_background(self ,wvs):
+        '''
+        A function that returns the sky background for a given set of wavelengths. 
+
+        Later it might be a function of pressure, temperature and humidity
+
+        Inputs: 
+        wvs     - A list of wavelengths 
+
+        Outputs: 
+        backgrounds - A list of backgrounds
+        '''
+        returnfloat = False
+        if isinstance(wvs, (float, int)):
+            returnfloat = True
+            wvs = np.array([wvs])
+        
+        # H band and shorter, just assume background is negligible. For now...
+        backgrounds = np.zeros(wvs.shape)
+
+        # Hack where I grabbed these from Allen's Astrophysical Quantities on sky brightness
+        # Very approximate step function. 
+        backgrounds[np.where((wvs > 1.8) & (wvs <= 2.3))] = 1e-6 # W/m^2/sr/um
+        backgrounds[np.where((wvs > 2.3) & (wvs <= 4))] = 1e-2 # W/m^2/sr/um
+        backgrounds[np.where(wvs > 4)] = 0.8 # W/m^2/sr/um
+
+        # convert to photons/s/cm^2/angstrom
+        diff_size = wvs * u.micron.to(u.m) / self.diameter # radians
+        psf_area = np.pi * (diff_size/2)**2 # sr
+
+        backgrounds *= (u.W / (constants.h * constants.c / (wvs * u.micron))).decompose().value * (u.m**-2/u.micron).to(u.cm**-2/u.angstrom) * psf_area
+
+        if returnfloat:
+            backgrounds = backgrounds[0]
+
+        return backgrounds
