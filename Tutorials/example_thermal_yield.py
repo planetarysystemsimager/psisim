Fixed units, made Keck backgrounds scalable in R

Tutorials/example_thermal_yield.py
@@ -3,16 +3,21 @@ import numpy as np
 import matplotlib.pylab as plt
 import copy
 import time
+import astropy.units as u
+import os
+import psisim
+
+psisim_path = os.path.dirname(psisim.__file__)
 
 tmt = telescope.TMT()
 psi_red = instrument.PSI_Red()
-psi_red.set_observing_mode(3600,2,'M',10, np.linspace(4.2,4.8,3)) #3600s, 2 exposures,M-band, R of 10
+psi_red.set_observing_mode(3600,2,'M',10, np.linspace(4.2,4.8,3)*u.micron) #3600s, 2 exposures,M-band, R of 10
 
 ######################################
 ######## Generate the universe #######
 ######################################
 
-exosims_config_filename = "forBruceandDimitri_EXOCAT1.json" #Some filename here
+exosims_config_filename = psisim_path+"/../Tutorials/forBruceandDimitri_EXOCAT1.json" #Some filename here
 uni = universe.ExoSims_Universe(exosims_config_filename)
 uni.simulate_EXOSIMS_Universe()
 
@@ -20,10 +25,10 @@ uni.simulate_EXOSIMS_Universe()
 ######## Lots of setup #######
 ##############################
 
-min_iwa = np.min(psi_red.current_wvs)*1e-6/tmt.diameter*206265
+min_iwa = np.min(psi_red.current_wvs).to(u.m)/tmt.diameter*u.rad
 planet_table = uni.planets
 # planet_table = planet_table[np.where(planet_table['PlanetMass'] > 10)]
-planet_table = planet_table[planet_table['AngSep']/1000 > min_iwa]
+planet_table = planet_table[planet_table['AngSep'] > min_iwa.to(u.mas)]
 planet_table = planet_table[planet_table['Flux Ratio'] > 1e-10]
 
 n_planets = len(planet_table)
@@ -54,14 +59,15 @@ n_model_wv = int((model_wv_high-model_wv_low)/(dwv_c/2))*2
 #Generate the model wavelenths
 model_wvs = np.linspace(model_wv_low, model_wv_high, n_model_wv) #Choose some wavelengths
 
+#Let's assume we're doing the AO sensing in I-band: 
+planet_table['StarAOmag'] = planet_table['StarImag']
+
 #####################################
 ######## Generate the Spectra #######
 #####################################
 
-
 print("\n Starting to generate planet spectra")
 for planet in planet_table[rand_planets]:
-
     #INSERT PLANET SELECTION RULES HERE
 
     if planet['PlanetMass'] < 10:
@@ -88,7 +94,6 @@ for planet in planet_table[rand_planets]:
     	## Generate the spectrum from cooling models and downsample to intermediate resolution
         atmospheric_parameters = age, 'M', True
         planet_spectrum = spectrum.simulate_spectrum(planet, model_wvs, intermediate_R, atmospheric_parameters, package='bex-cooling')
-
 
         ## Generate the spectrum from a blackbody
         atmospheric_parameters = 0.5#The bond albedo

Tutorials/example_yield_calculation.py
@@ -5,10 +5,11 @@ import matplotlib.pylab as plt
 import copy
 import time
 from astropy.io import fits
+import astropy.units as u
 
 tmt = telescope.TMT()
 psi_blue = instrument.PSI_Blue()
-psi_blue.set_observing_mode(3600,10,'z',50, np.linspace(0.60,0.85,40)) #60s, 40 exposures,z-band, R of 10
+psi_blue.set_observing_mode(3600,10,'z',50, np.linspace(0.60,0.85,40)*u.micron) #60s, 40 exposures,z-band, R of 10
 
 exosims_config_filename = "forBruceandDimitri_EXOCAT1.json" #Some filename here
 uni = universe.ExoSims_Universe(exosims_config_filename)
@@ -17,15 +18,15 @@ uni.simulate_EXOSIMS_Universe()
 planet_table = uni.planets
 full_planet_table = copy.deepcopy(uni.planets)
 #Down select the planets whose separations are less than lambda/D
-min_iwa = np.min(psi_blue.current_wvs)*1e-6/tmt.diameter*206265
-planet_table = planet_table[planet_table['AngSep']/1000 > min_iwa]
+min_iwa = np.min(psi_blue.current_wvs).to(u.m)/tmt.diameter*u.rad
+planet_table = planet_table[planet_table['AngSep']/1000 > min_iwa.to(u.mas)]
 planet_table = planet_table[planet_table['Flux Ratio'] > 1e-10]
 n_planets = len(planet_table)
 
 planet_types = []
 planet_spectra = []
 
-n_planets_now = 2000
+n_planets_now = 2
 #We'll pick random planets, since many systems are multi-planet systems and they show up 
 #sequentially in EXOSIMS
 rand_planets = np.random.randint(0, n_planets, n_planets_now)
@@ -75,10 +76,10 @@ def generate_spectrum(planet):
     return planet_type, planet_spectrum
 
 ### Non parallel Code 
-# for planet in planet_table[rand_planets]:
-#     planet_type, planet_spectrum = generate_spectrum(planet)
-#     planet_types.append(planet_type)
-#     planet_spectra.append(planet_spectrum)
+for planet in planet_table[rand_planets]:
+    planet_type, planet_spectrum = generate_spectrum(planet)
+    planet_types.append(planet_type)
+    planet_spectra.append(planet_spectrum)
 
 ### Parallel Code
 pool = mp.Pool(processes=2) # pick the number of processes you want to use

psisim/observation.py
@@ -50,10 +50,14 @@ def simulate_observation(telescope,instrument,planet_table_entry,planet_spectrum
     stellar_spectrum *= telescope.collecting_area.to(u.cm**2)
 
     #Multiply by atmospheric transmission
-    stellar_spectrum *= telescope.get_atmospheric_transmission(wvs)
+    stellar_spectrum *= telescope.get_atmospheric_transmission(wvs,R=instrument.current_R)
 
     #Multiply by telescope throughput
     stellar_spectrum *= telescope.get_telescope_throughput(wvs,instrument)
+
+
+    ## TODO: pass in the planet separation to get_inst_throughput
+    ## TODO: make a planet and star flag for get_inst_throughput (may depend on separation)
 
     #Multiply by instrument throughputs and quantum efficiency
     stellar_spectrum *= instrument.get_inst_throughput(wvs)
@@ -66,7 +70,7 @@ def simulate_observation(telescope,instrument,planet_table_entry,planet_spectrum
     scaled_spectrum = planet_spectrum*stellar_spectrum
 
     #Get Sky thermal background in photons/s/Angstrom
-    thermal_sky = telescope.get_sky_background(wvs) #Assumes diffraction limited PSF had to multiply by solid angle of PSF.
+    thermal_sky = telescope.get_sky_background(wvs,R=instrument.current_R) #Assumes diffraction limited PSF had to multiply by solid angle of PSF.
     thermal_sky *= telescope.collecting_area.to(u.cm**2) #Multiply by collecting area - units of photons/s/Angstom
     thermal_sky *= telescope.get_telescope_throughput(wvs,band=instrument.current_filter) #Multiply by telescope throughput
     thermal_sky *= instrument.get_inst_throughput(wvs) #Multiply by instrument throughput
@@ -160,6 +164,7 @@ def simulate_observation(telescope,instrument,planet_table_entry,planet_spectrum
         # equal to the noise
         for i,noise in enumerate(total_noise):
             # import pdb; pdb.set_trace()
+            ## TODO: Make this poisson so that it's valid still in low photon count regime. 
             detector_spectrum[i] = np.random.normal(detector_spectrum[i],noise)
 
     #TODO: Currently everything is in e-. We likely want it in a different unit at the end. 
@@ -336,8 +341,6 @@ def get_noise_components(separation,star_aomag,instrument,wvs,star_spt,stellar_s
     Calculate all of the different noise contributions
     '''
 
-    #### TODO include photon noise from the speckles
-    
     # First is speckle noise.
     # Instrument.get_speckle_noise should return things in contrast units relative to the star
     speckle_noise = instrument.get_speckle_noise(separation,star_aomag,instrument.current_filter,wvs,star_spt,telescope)[0]

psisim/spectrum.py
@@ -11,18 +11,29 @@ import copy
 from scipy.ndimage.interpolation import shift
 from scipy.ndimage import gaussian_filter
 
+try: 
+    import pysynphot as ps
+except ImportError:
+    pass
 
-# try:
-#     bex_labels = ['Age', 'Mass', 'Radius', 'Luminosity', 'Teff', 'Logg', 'NACOJ', 'NACOH', 'NACOKs', 'NACOLp', 'NACOMp', 'CousinsR', 'CousinsI', 'WISE1', 'WISE2', 'WISE3', 'WISE4', 
-#                 'F115W', 'F150W', 'F200W', 'F277W', 'F356W', 'F444W', 'F560W', 'F770W', 'F1000W', 'F1280W', 'F1500W', 'F1800W', 'F2100W', 'F2550W', 'VISIRB87', 'VISIRSiC', 
-#                 'SPHEREY', 'SPHEREJ', 'SPHEREH', 'SPHEREKs', 'SPHEREJ2', 'SPHEREJ3', 'SPHEREH2', 'SPHEREH3', 'SPHEREK1', 'SPHEREK2']
-#     # initalize on demand when needed
-#     bex_cloudy_mh0 = {}
-#     bex_clear_mh0 = {}
 
+bex_labels = ['Age', 'Mass', 'Radius', 'Luminosity', 'Teff', 'Logg', 'NACOJ', 'NACOH', 'NACOKs', 'NACOLp', 'NACOMp', 'CousinsR', 'CousinsI', 'WISE1', 'WISE2', 'WISE3', 'WISE4', 
+            'F115W', 'F150W', 'F200W', 'F277W', 'F356W', 'F444W', 'F560W', 'F770W', 'F1000W', 'F1280W', 'F1500W', 'F1800W', 'F2100W', 'F2550W', 'VISIRB87', 'VISIRSiC', 
+            'SPHEREY', 'SPHEREJ', 'SPHEREH', 'SPHEREKs', 'SPHEREJ2', 'SPHEREJ3', 'SPHEREH2', 'SPHEREH3', 'SPHEREK1', 'SPHEREK2']
+# initalize on demand when needed
+bex_cloudy_mh0 = {}
+bex_clear_mh0 = {}
+
+
+# try: 
+#     import picaso
+#     from picaso import justdoit as jdi
 #     opacity_folder = os.path.join(os.path.dirname(picaso.__file__), '..', 'reference', 'opacities')
 #     dbname = "opacity_LR.db"
 #     opacity = jdi.opannection(os.path.join(opacity_folder, dbname))
+# except ImportError:
+#     pass
+
 
 def generate_picaso_inputs(planet_table_entry, planet_type, clouds=True, planet_mh=1, stellar_mh=0.0122, planet_teq=None, verbose=False):
     '''
@@ -41,8 +52,7 @@ def generate_picaso_inputs(planet_table_entry, planet_type, clouds=True, planet_
     Outputs:
     params - picaso.justdoit.inputs class
     '''
-    import picaso
-    from picaso import justdoit as jdi
+    
 
     global opacity
 
@@ -237,15 +247,17 @@ def simulate_spectrum(planet_table_entry, wvs, R, atmospheric_parameters, packag
 
     elif package.lower() == "blackbody":
         a_v = atmospheric_parameters # just albedo
-        pl_teff = ((1 - a_v)/4  * (planet_table_entry['StarRad'] * u.solRad/(planet_table_entry['SMA'] * u.au)).decompose()**2 * planet_table_entry['StarTeff']**4)**(1./4)
+        pl_teff = ((1 - a_v)/4  * (planet_table_entry['StarRad'] * u.solRad/(planet_table_entry['SMA'])).decompose()**2 * planet_table_entry['StarTeff']**4)**(1./4)
 
-        nu = consts.c/(wvs * u.micron) # freq
+        nu = consts.c/(wvs) # freq
         bb_arg_pl = (consts.h * nu/(consts.k_B * pl_teff * u.cds.K)).decompose()
         bb_arg_star = (consts.h * nu/(consts.k_B * planet_table_entry['StarTeff'] * u.cds.K)).decompose()
+
         thermal_flux_ratio = ((planet_table_entry['PlanetRadius'] * u.earthRad)/(planet_table_entry['StarRad'] * u.solRad)).decompose()**2 * np.expm1(bb_arg_star)/np.expm1(bb_arg_pl)
         
-        phi = (np.sin(planet_table_entry['Phase']) + (np.pi - planet_table_entry['Phase'])*np.cos(planet_table_entry['Phase']))/np.pi
-        reflected_flux_ratio = phi * a_v / 4 * (planet_table_entry['PlanetRadius'] * u.earthRad/(planet_table_entry['SMA'] * u.au)).decompose()**2
+        #Lambertian? What is this equation - To verify later. 
+        phi = (np.sin(planet_table_entry['Phase']) + (np.pi - planet_table_entry['Phase'].to(u.rad).value)*np.cos(planet_table_entry['Phase']))/np.pi
+        reflected_flux_ratio = phi * a_v / 4 * (planet_table_entry['PlanetRadius'] * u.earthRad/(planet_table_entry['SMA'])).decompose()**2
 
         return thermal_flux_ratio + reflected_flux_ratio
 
@@ -289,7 +301,7 @@ def get_stellar_spectrum(planet_table_entry,wvs,R,model='Castelli-Kurucz',verbos
     '''
 
     if model == 'pickles':
-        import pysynphot as ps
+        # import pysynphot as ps
         #Get the pickles spectrum in units of photons/s/cm^2/angstrom. 
         #Wavelength units are microns
         sp = get_pickles_spectrum(planet_table_entry['StarSpT'],verbose=verbose)
@@ -317,7 +329,6 @@ def get_stellar_spectrum(planet_table_entry,wvs,R,model='Castelli-Kurucz',verbos
         stellar_spectrum = np.array(stellar_spectrum)
     
     elif model == 'Castelli-Kurucz':
-        import pysynphot as ps
         # For now we're assuming a metallicity of 0, because exosims doesn't
         # provide anything different
 
@@ -355,7 +366,7 @@ def get_stellar_spectrum(planet_table_entry,wvs,R,model='Castelli-Kurucz',verbos
             dwvs = sp_norm.wave - np.roll(sp_norm.wave, 1)
             dwvs[0] = dwvs[1]
             #Pick the index closest to our wavelength. 
-            ind = np.argsort(np.abs((sp_norm.wave-wv)))[0]
+            ind = np.argsort(np.abs((sp_norm.wave*u.micron-wv)))[0]
             dwv = dwvs[ind]
 
             R_in = wv/dwv

psisim/telescope.py
@@ -1,7 +1,9 @@
 import numpy as np
+import scipy.interpolate as si
 import astropy.units as u
 import astropy.constants as constants
 from astropy.modeling.blackbody import blackbody_lambda, blackbody_nu
+from psisim import spectrum
 
 class Telescope():
     '''
@@ -86,52 +88,111 @@ class Telescope():
         else:
             return np.ones(len(wvs))
 
-
-
 class TMT(Telescope):
     '''
     An implementation of the Telescope class
     '''
-    def __init__(self):
+    def __init__(self,airmass = 1.0,water_vapor=  1.0):
         super(TMT, self).__init__(30)
 
-    def get_sky_background(self ,wvs):
+        self.temperature = 276 * u.K
+        self.median_seeing = 0.6 * u.arcsec
+        self.airmass = airmass
+        self.water_vapor = water_vapor
+
+    def get_sky_background(self, wvs, path="/scr3/dmawet/ETC/",R=1e5):
         '''
         A function that returns the sky background for a given set of wavelengths. 
 
-        Later it might be a function of pressure, temperature and humidity
+        #Based on Keck sky backgrounds done by Dimitri - Currently super high resolution
 
         Inputs: 
-        wvs     - A list of wavelengths 
+        wvs     - A list of wavelengths assumed to be microns
 
         Outputs: 
         backgrounds - A list of backgrounds
         '''
-        returnfloat = False
-        if isinstance(wvs, (float, int)):
-            returnfloat = True
-            wvs = np.array([wvs])
+
+        #Calculate some stuff
+        diffraction_limit = (wvs/self.diameter.to(u.micron)*u.radian).to(u.arcsec)
+        solidangle = diffraction_limit**2 * 1.13
+
+        #Read in the background file
+        sky_background_tmp = np.genfromtxt(path+'sky/mk_skybg_zm_'+str(self.water_vapor)+'_'+str(self.airmass)+'_ph.dat', skip_header=0)
+        sky_background_MK = sky_background_tmp[:,1]
+        sky_background_MK_wave = sky_background_tmp[:,0] * u.nm
+
+        #Interpolate it to the wavelengths we care about
+        # sky_background = np.interp(wvs,sky_background_MK_wave.to(u.micron),sky_background_MK)*u.photon/(u.s*u.arcsec**2*u.nm*u.m**2) 
+        sky_background = si.interp1d(wvs,sky_background_MK_wave.to(u.micron),sky_background_MK,bounds_error=False,fill_value='extrapolate')*u.photon/(u.s*u.arcsec**2*u.nm*u.m**2) 
+
+        if R < 1e5:
+            sky_background = spectrum.downsample_spectrum(sky_background,1e5,R)
+
+        #Multiply by the solid angle
+        sky_background *= solidangle
+
+        #Return the function in units that we like. 
+        return sky_background.to(u.ph/u.s/u.cm**2/u.AA,equivalencies=u.spectral_density(wvs))
+
+    def get_atmospheric_transmission(self,wave,path="/scr3/dmawet/ETC/",R=1e5):
+        '''
+        A function that computes the sky and telescope throughput 
+
+        Arguments 
+        ----------
+        wave     - A single wavelength or array of wavelengths [microns]
+        path    - The path we we can find the transmission files
+        '''
+
+        #Read in the sky transmission for the current observing conditions
+        sky_trans_tmp = np.genfromtxt(path+'sky/mktrans_zm_'+str(self.water_vapor)+'_'+str(self.airmass)+'.dat', skip_header=0)
+        sky_trans = sky_trans_tmp[:,1]
+        sky_trans_wave = sky_trans_tmp[:,0]*u.micron #* u.nm
+
+        #Interpolate to the wavelengths that we want. 
+        # sky_trans_interp = np.interp(wave,sky_trans_wave,sky_trans)
+        sky_trans_interp = si.interp1d(wave,sky_trans_wave,sky_trans,bounds_error=False,fill_value='extrapolate')
+
+        if R < 1e5:
+            sky_trans_interp = spectrum.downsample_spectrum(sky_trans_interp,1e5,R)
+
+        return sky_trans_interp
+
+    def get_telescope_throughput(self,wvs,band="TwoMass-J"):
+        '''
+        Get Telescope throughput for a given observing band.  - These are Keck values
+
+        Currently only Y,TwoMASS-J,TwoMASS-H and TwoMASS-K are supported, otherwise 0.88 is returned. 
+
+        Args:
+            band (str): A photometric band. 
         
-        # H band and shorter, just assume background is negligible. For now...
-        backgrounds = np.zeros(wvs.shape)
+        '''
 
-        # Hack where I grabbed these from Allen's Astrophysical Quantities on sky brightness
-        # Very approximate step function. 
-        backgrounds[np.where((wvs > 1.8) & (wvs <= 2.3))] = 1e-6 # W/m^2/sr/um
-        backgrounds[np.where((wvs > 2.3) & (wvs <= 4))] = 1e-2 # W/m^2/sr/um
-        backgrounds[np.where(wvs > 4)] = 0.8 # W/m^2/sr/um
+        throughput = {'Y':0.88,"TwoMASS-J":0.88,"TwoMASS-H":0.88,"TwoMASS-K":0.88}.get(band,0.88)
+        
+        return throughput*np.ones(np.shape(wvs))
 
-        # convert to photons/s/cm^2/angstrom
-        diff_size = wvs * u.micron.to(u.m) / self.diameter # radians
-        psf_area = np.pi * (diff_size/2)**2 # sr
+    def get_thermal_emission(self,wvs,band="TwoMass-J"):
+        '''
+        The telescope emission as a function of wavelength
 
-        backgrounds *= (u.W / (constants.h * constants.c / (wvs * u.micron))).decompose().value * (u.m**-2/u.micron).to(u.cm**-2/u.angstrom) * psf_area
+        Outputs:
+        thermal_emission - usnits of photons/s/cm**2/angstrom
+        '''
 
-        if returnfloat:
-            backgrounds = backgrounds[0]
+        diffraction_limit = (wvs/self.diameter.to(u.micron)*u.radian).to(u.arcsec)
+        solidangle = diffraction_limit**2 * 1.13
 
-        return backgrounds
+        thermal_emission = blackbody_lambda(wvs,self.temperature)
+        thermal_emission *= solidangle
+        thermal_emission = thermal_emission.to(u.ph/(u.s * u.cm**2 * u.AA),equivalencies=u.spectral_density(wvs))
 
+        thermal_emission *= self.get_telescope_emissivity(wvs,band=band)
+
+        return thermal_emission
+        
 class Keck(Telescope):
     '''
     An implementation of the Telescope class

psisim/universe.py
@@ -55,7 +55,7 @@ class ExoSims_Universe(Universe):
         flux_ratios = 10**(sim.SimulatedUniverse.dMag/-2.5)  # grab for now from EXOSIMS
         angseps = sim.SimulatedUniverse.WA.value * 1000 *u.mas # mas
         projaus = sim.SimulatedUniverse.d.value * u.AU # au
-        phase = np.arccos(sim.SimulatedUniverse.r[:,2]/sim.SimulatedUniverse.d) *u.rad# planet phase  [0, pi]
+        phase = np.arccos(sim.SimulatedUniverse.r[:,2]/sim.SimulatedUniverse.d)# planet phase  [0, pi]
         smas = sim.SimulatedUniverse.a.value*u.AU # au
         eccs = sim.SimulatedUniverse.e # eccentricity
         incs = sim.SimulatedUniverse.I.value*u.deg # degrees