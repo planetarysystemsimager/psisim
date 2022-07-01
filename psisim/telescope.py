83bb31bc2d0633a7561db31abf28cf30dc9f41a0
jeaadams <jea.iyanla@gmail.com>
2022-07-01 14:47:51 -0400

    Create GMT telescope

--- psisim/telescope.py
+++ psisim/telescope.py
@@ -382,3 +382,130 @@ class Keck(Telescope):
 
 
     
+
+class GMT(Telescope):
+    '''
+    An implementation of the Telescope class
+    '''
+    def __init__(self,airmass = 1.0,water_vapor=  1.0,path=None):
+        super(TMT, self).__init__(24.5)
+
+        self.temperature = 276 * u.K
+        self.median_seeing = 0.6 * u.arcsec
+        self.airmass = airmass
+        self.water_vapor = water_vapor
+
+        if path is None:
+            path = datadir
+        self.path = path #A path to background, transmission and AO files
+
+    def get_sky_background(self, wvs, R=1e5):
+        '''
+        A function that returns the sky background for a given set of wavelengths. 
+
+        #Based on Keck sky backgrounds done by Dimitri - Currently super high resolution
+
+        Inputs: 
+        wvs     - A list of wavelengths assumed to be microns
+
+        Outputs: 
+        backgrounds - A list of backgrounds
+        '''
+
+        #Calculate some stuff
+        diffraction_limit = (wvs/self.diameter.to(u.micron)*u.radian).to(u.arcsec)
+        solidangle = diffraction_limit**2 * 1.13
+
+        #Read in the background file
+        sky_background_tmp = np.genfromtxt(self.path+'sky/mk_skybg_zm_'+str(self.water_vapor)+'_'+str(self.airmass)+'_ph.dat', skip_header=0)
+        sky_background_MK = sky_background_tmp[:,1]
+        sky_background_MK_wave = sky_background_tmp[:,0] * u.nm
+
+        #Interpolate it to the wavelengths we care about
+        # sky_background = np.interp(wvs,sky_background_MK_wave.to(u.micron),sky_background_MK)*u.photon/(u.s*u.arcsec**2*u.nm*u.m**2) 
+        sky_background = si.interp1d(sky_background_MK_wave.to(u.micron).value,sky_background_MK,bounds_error=False,fill_value='extrapolate')(wvs)*u.photon/(u.s*u.arcsec**2*u.nm*u.m**2)
+
+        if R < 1e5:
+            tmp_spec = spectrum.Spectrum(wvs, sky_background, 1e5)
+            sky_background = tmp_spec.downsample_spectrum(R) * sky_background.unit
+
+        #Multiply by the solid angle
+        sky_background *= solidangle
+
+        #Return the function in units that we like. 
+        return sky_background.to(u.ph/u.s/u.cm**2/u.AA,equivalencies=u.spectral_density(wvs))
+
+    def get_atmospheric_transmission(self,wave,R=1e5):
+        '''
+        A function that computes the sky transmission as a function of wavelength
+
+        Arguments 
+        ----------
+        wave     - A single wavelength or array of wavelengths [microns]
+        '''
+
+        #Read in the sky transmission for the current observing conditions
+        sky_trans_tmp = np.genfromtxt(self.path+'sky/mktrans_zm_'+str(self.water_vapor)+'_'+str(self.airmass)+'.dat', skip_header=0)
+        sky_trans = sky_trans_tmp[:,1]
+        sky_trans_wave = sky_trans_tmp[:,0]*u.micron #* u.nm
+
+        #Interpolate to the wavelengths that we want. 
+        # sky_trans_interp = np.interp(wave,sky_trans_wave,sky_trans)
+        sky_trans_interp = si.interp1d(sky_trans_wave,sky_trans,bounds_error=False,fill_value='extrapolate')(wave)
+
+        if R < 1e5:
+            tmp_spec = spectrum.Spectrum(wave, sky_trans_interp, 1e5)
+            sky_trans_interp = tmp_spec.downsample_spectrum(R)
+
+        return sky_trans_interp
+
+    def get_telescope_throughput(self,wvs,band="TwoMass-J"):
+        '''
+        Get Telescope throughput for a given observing band.  
+
+        Currently only Y,TwoMASS-J,TwoMASS-H and TwoMASS-K are supported, otherwise 0.91 is returned. 
+
+        Args:
+            band (str): A photometric band. 
+        
+        '''
+        # From Dimitri's Code
+        throughput = {"CFHT-Y":0.91,"TwoMASS-J":0.91,"TwoMASS-H":0.91,"TwoMASS-K":0.91}.get(band,0.91)
+        
+        return throughput*np.ones(np.shape(wvs))
+    
+    def get_telescope_emissivity(self,wvs,band="TwoMass-J"):
+        '''
+        Get Telescope emissivity for a given observing band. 
+
+        Currently only Y,J,H and K are supported, otherwise 1-0.88 is returned. 
+
+        Args:
+            band (str): A photometric band. 
+        
+        '''
+        
+        emissivity = 1-self.get_telescope_throughput(wvs,band=band)
+
+        return emissivity
+    
+    def get_thermal_emission(self,wvs,band="TwoMass-J"):
+        '''
+        The telescope emission as a function of wavelength
+
+        Outputs:
+        thermal_emission - usnits of photons/s/cm**2/angstrom
+        '''
+
+        diffraction_limit = (wvs/self.diameter.to(u.micron)*u.radian).to(u.arcsec)
+        solidangle = diffraction_limit**2 * 1.13
+
+        bb_lam = BlackBody(self.temperature,scale=1.0*u.erg/(u.cm**2*u.AA*u.s*u.sr))
+        thermal_emission = bb_lam(wvs)
+        thermal_emission *= solidangle
+        thermal_emission = thermal_emission.to(u.ph/(u.s * u.cm**2 * u.AA),equivalencies=u.spectral_density(wvs))
+
+        thermal_emission *= self.get_telescope_emissivity(wvs,band=band)
+
+        return thermal_emission
+  
